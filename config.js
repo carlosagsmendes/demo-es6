43346d8a6eb465376f565df3e6d8b004469f23e7
Carlos Mendes <carlos.ags.mendes@gmail.com>
2015-07-18 22:18:04 +0100

    Updated to JSPM 16 beta 3

--- config.js
+++ config.js
@@ -1,14 +1,34 @@
 System.config({
+  "defaultJSExtensions": true,
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
   "paths": {
-    "*": "*.js",
-    "github:*": "https://github.jspm.io/*.js",
-    "npm:*": "https://npm.jspm.io/*.js"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   }
 });
 
 System.config({
   "map": {
-    "jquery": "github:components/jquery@2.1.1"
+    "babel": "npm:babel-core@5.7.4",
+    "babel-runtime": "npm:babel-runtime@5.7.0",
+    "core-js": "npm:core-js@0.9.18",
+    "jquery": "github:components/jquery@2.1.1",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.7.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
   }
 });
 
