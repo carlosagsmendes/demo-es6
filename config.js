b8d8bece84c7404bee9cbec0502a6bfcb6d95ce7
Carlos Mendes <carlos.ags.mendes@gmail.com>
2015-07-21 19:53:49 +0100

    Jquery FileUpload

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
     ]
   },
   "paths": {
+    "*": "lib/*",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   }
@@ -17,10 +18,14 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.7.4",
     "babel-runtime": "npm:babel-runtime@5.7.0",
+    "blueimp/jQuery-File-Upload": "github:blueimp/jQuery-File-Upload@9.10.4",
     "core-js": "npm:core-js@0.9.18",
     "jquery": "github:components/jquery@2.1.4",
     "jquery-ui": "github:components/jqueryui@1.11.4",
     "knockout": "github:knockout/knockout@3.3.0",
+    "github:blueimp/jQuery-File-Upload@9.10.4": {
+      "jquery-ui": "github:components/jqueryui@1.11.4"
+    },
     "github:components/jqueryui@1.11.4": {
       "jquery": "github:components/jquery@2.1.4"
     },
